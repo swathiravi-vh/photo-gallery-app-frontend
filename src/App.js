8b8bb5f2ea7ae3b7f25a1576c0f0576c6e8d7c35
swathiravi-vh <swathiit01@gmail.com>
2023-05-10 13:24:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Button from "./components/Button";
+import Grid from "./components/Grid";
+import Navbar from "./components/Navbar";
+import axios from "axios";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+  const [updateUI, setUpdateUI] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/get")
+      .then((res) => {
+        console.log(res.data);
+        setPhotos(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, [updateUI]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Grid photos={photos} />
+      <Button setUpdateUI={setUpdateUI} />
     </div>
   );
 }
